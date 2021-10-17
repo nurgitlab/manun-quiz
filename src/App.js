6ba57bdf6c283029c22dfad62a881a34e486b6b2
nurgitlab <nurbek.math@gmail.com>
2021-10-17 12:18:11 +0300

    random generation added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { MainPage } from "./pages/MainPage";
 import { StartQuiz } from "./pages/StartQuiz";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 
 export const App = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route
-          path={"/"}
-          exact={true}
-        >
-          <div>
-            Manchester
-            <br/>
-            <MainPage />
-          </div>
-        </Route>
+  const defaultState = {
+    easyQuestions: []
+  };
 
-        <Route
-          path={"/questions"}
-          exact={true}
-        >
-          <div>
-            Manchester
-            <br/>
-            <StartQuiz />
-          </div>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+  const reducer = (state = defaultState, action) => {
+    if (action.type === "IMPORT_ALL_QUESTIONS") {
+      return {...state, easyQuestions: action.newQuestions};
+    } else if (action.type === "IMPORT_RANDOM_QUESTIONS") {
+      let randomQuestions = [];
+      for (let i = 0; i < 3; i++) {
+        let randomNum = Math.floor(Math.random() * (action.newQuestions.easyQuestions.length - i)) + i;
+        let mem = action.newQuestions.easyQuestions[randomNum];
+        action.newQuestions.easyQuestions[randomNum] = action.newQuestions.easyQuestions[i];
+        action.newQuestions.easyQuestions[i] = mem;
+        randomQuestions.push(mem);
+      }
+      return {...state, easyQuestions: {easyQuestions: randomQuestions}};
+    } else {
+      return state;
+    }
+  };
+
+  const store = createStore(reducer);
+
+  return (
+    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path={"/"}
+              exact={true}
+            >
+              <div>
+                Manchester
+                <br/>
+                <MainPage/>
+              </div>
+            </Route>
+
+            <Route
+              path={"/questions"}
+              exact={true}
+            >
+              <div>
+                Manchester
+                <br/>
+                <StartQuiz/>
+              </div>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </div>
   );
 }
 
--- src/pages/StartQuiz.js
+++ src/pages/StartQuiz.js
@@ -1,21 +1,49 @@
 import myQuestions from "../db.json";
+import { useDispatch, useSelector } from "react-redux";
 
 
 export const StartQuiz = () => {
   const obj = myQuestions;
   console.log(obj);
+
   // Вот тут в целом вопросы можно вызвать и через AXIOS с сервера, заглушка
 
   // Тут я должен положить файлы в REDUX!!!
+  const dispatch = useDispatch();
+  const addQuestionsToRedux = () => {
+    dispatch({
+      type: "IMPORT_RANDOM_QUESTIONS",
+      newQuestions: obj,
+    });
+  };
+
+  const currentQuestions = useSelector(state => state.easyQuestions);
+  console.log(currentQuestions);
   return (
     <div>
       Start Quiz Page
       <br/>
+      <button onClick={addQuestionsToRedux}>SHOW QUESTIONS</button>
+      <br/>
       <div>
         <ul>
-          {obj.easyQuestions.map((question) => {
-
-          })}
+          {currentQuestions.easyQuestions ? currentQuestions.easyQuestions.map((question, id) => {
+            return (
+              <li key={id}>
+                {question.question}
+                <br/>
+                <ul>
+                  {question.answers.map((answer, id) => {
+                    return (
+                      <li key={id}>
+                        {answer}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          }) : (<div>SSH</div>)}
         </ul>
       </div>
     </div>
