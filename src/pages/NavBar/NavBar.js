b2a013b539dce686fb8b0057d29882181e2f2218
nurgitlab <nurbek.math@gmail.com>
2021-10-21 14:26:34 +0300

    crush bug with routings in questions fixed

--- src/pages/NavBar/NavBar.js
+++ src/pages/NavBar/NavBar.js
@@ -1,10 +1,20 @@
 import "./NavBar.css"
+import { useHistory } from "react-router";
 
 
 export const NavBar = () => {
+  const history = useHistory()
+
+  const goToMain = () => {
+    history.push("")
+  }
   return (
     <div className={"main-div"}>
-      <div className={"logo"}></div>
+      <div
+        className={"logo"}
+        onClick={()=>goToMain()}
+      >
+      </div>
       <div className={"nav-button"}>Главная страница</div>
       <div className={"nav-button"}>О проекте</div>
       <div className={"nav-button"}>Контакты</div>
--- src/pages/QuestonShow/QuestionShow.js
+++ src/pages/QuestonShow/QuestionShow.js
@@ -4,18 +4,22 @@ import "./QuestionShow.css";
 
 
 export const QuestionShow = ({questionsId}) => {
-  const history = useHistory()
-  const dispatch = useDispatch()
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-  const currentQuestions = useSelector(state => state.easyQuestions)
+  const currentQuestions = useSelector(state => state.easyQuestions);
+
+  console.log(currentQuestions.length);
+  console.log(currentQuestions);
+  console.log("HERE");
 
   const goToNextQuestion = () => {
-    history.push(`/questions/${Number(questionsId)+1}`)
-  }
+    history.push(`/questions/${Number(questionsId) + 1}`);
+  };
 
   const goToPrevQuestion = () => {
-    history.push(`/questions/${Number(questionsId)-1}`)
-  }
+    history.push(`/questions/${Number(questionsId) - 1}`);
+  };
 
   const addAnswer = (answer, question, qId) => {
     dispatch({
@@ -33,41 +37,48 @@ export const QuestionShow = ({questionsId}) => {
   };
 
   const goToFinalPage = () => {
-    history.push(`/final`)
+    history.push(`/final`);
   };
 
   return (
     <div>
-      {questionsId}
-      <br/>
-      {counterOfCorrectAnswers}
-      <br/>
-      {questionsId == 0 ?
-        <button onClick={goToPrevModule}>GO BACK</button> :
-        <button onClick={goToPrevQuestion}>PREVIOUS QUESTION</button>
+      {(currentQuestions.length) !== 0 ?
+        <div>
+          {questionsId}
+          <br/>
+          {counterOfCorrectAnswers}
+          <br/>
+          {questionsId == 0 ?
+            <button onClick={goToPrevModule}>GO BACK</button> :
+            <button onClick={goToPrevQuestion}>PREVIOUS QUESTION</button>
+          }
+          {questionsId == (currentQuestions.easyQuestions.length - 1) ?
+            <button onClick={goToFinalPage}>FINISH</button> :
+            <button onClick={goToNextQuestion}>NEXT QUESTION</button>
+          }
+          <br/>
+          <span className={"some-class"}>{currentQuestions.easyQuestions[questionsId].question}</span>
+          <br/>
+          <ul>
+            {currentQuestions.easyQuestions[questionsId].answers.map((answer, id) => {
+              return (
+                <li key={id}>
+                  <button onClick={() => addAnswer(
+                    answer,
+                    currentQuestions.easyQuestions[questionsId],
+                    questionsId)
+                  }>
+                    {answer}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div> :
+        <div>
+          404
+        </div>
       }
-      {questionsId == (currentQuestions.easyQuestions.length - 1) ?
-        <button onClick={goToFinalPage}>FINISH</button> :
-        <button onClick={goToNextQuestion}>NEXT QUESTION</button>
-      }
-      <br/>
-      <span className={"some-class"}>{currentQuestions.easyQuestions[questionsId].question}</span>
-      <br/>
-      <ul>
-        {currentQuestions.easyQuestions[questionsId].answers.map((answer, id) => {
-          return (
-            <li key={id}>
-              <button onClick={() => addAnswer(
-                answer,
-                currentQuestions.easyQuestions[questionsId],
-                questionsId)
-              }>
-                {answer}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
     </div>
-  )
-}
+  );
+};
