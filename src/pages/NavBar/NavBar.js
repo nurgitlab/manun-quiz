8fa2c4548269928a9e053129326609a694c9f58e
nurgitlab <nurbek.math@gmail.com>
2021-10-21 03:18:28 +0300

    animation on navbar buttons added

--- src/pages/NavBar/NavBar.js
+++ src/pages/NavBar/NavBar.js
@@ -4,10 +4,10 @@ import "./NavBar.css"
 export const NavBar = () => {
   return (
     <div className={"main-div"}>
-      <div className={"nav-button"}>HERE WILL BE LOGO</div>
-      <div className={"nav-button"}>Main Page</div>
-      <div className={"nav-button"}>About the project</div>
-      <div className={"nav-button"}>Contacts</div>
+      <div className={"logo"}></div>
+      <div className={"nav-button"}>Главная страница</div>
+      <div className={"nav-button"}>О проекте</div>
+      <div className={"nav-button"}>Контакты</div>
     </div>
   )
 }
