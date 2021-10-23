00db5c79f932cc95d2767e5f6b174b8c2f49e3ad
nurgitlab <nurbek.math@gmail.com>
2021-10-23 17:40:40 +0300

    crash on final page reload fixed

--- src/pages/FinalPage/FinalPage.js
+++ src/pages/FinalPage/FinalPage.js
@@ -2,6 +2,7 @@ import { useHistory } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import React from "react";
 import "./FinalPage.css";
+import { ErrorPage } from "../ErrorPage/ErrorPage";
 
 
 export const FinalPage = () => {
@@ -9,17 +10,25 @@ export const FinalPage = () => {
   const dispatch = useDispatch();
 
   const counterOfCorrectAnswers = useSelector(state => state.counter);
-  const numberOfQuestions = useSelector(state => state.easyQuestions.easyQuestions.length);
   const allQuestions = useSelector(state => state.easyQuestions.easyQuestions);
-  const [usersMistakes, setUsersMistakes] = React.useState([])
+
+  let numberOfQuestions;
+
+  try {
+    numberOfQuestions = allQuestions.length;
+  } catch (e) {
+    numberOfQuestions = -1;
+  }
+
+  const [usersMistakes, setUsersMistakes] = React.useState([]);
 
   const goBackToMain = () => {
     history.push("/");
   };
 
   const showMistakes = () => {
-    setUsersMistakes([])
-    let memIncorrectArray = []
+    setUsersMistakes([]);
+    let memIncorrectArray = [];
     allQuestions.map((question) => {
       if (question.correctAnswer != question.usersAnswer) {
         if (question.usersAnswer == "") {
@@ -36,85 +45,93 @@ export const FinalPage = () => {
   };
 
   return (
-    <div
-      className={"final-main-block"}
-    >
-      <div
-        className={"final-info"}
-      >
-        Поздравляем!
-      </div>
-      <div
-        className={"final-info"}
-      >
-        Вы прошли U.N.I.T.E.D. тест и набрали
-        <span
-          className={"big-font"}
+    <div>
+      {(numberOfQuestions) > 0 ? (
+        <div
+          className={"final-main-block"}
         >
+          <div
+            className={"final-info"}
+          >
+            Поздравляем!
+          </div>
+          <div
+            className={"final-info"}
+          >
+            Вы прошли U.N.I.T.E.D. тест и набрали
+            <span
+              className={"big-font"}
+            >
           {" " + counterOfCorrectAnswers + " "}
         </span>
-        из
-        <span
-          className={"big-font"}
-        >
+            из
+            <span
+              className={"big-font"}
+            >
           {" " + numberOfQuestions + " "}
         </span>
-        очков.
-      </div>
-
-      <div
-        onClick={() => goBackToMain()}
-        className={"start-button"}
-      >
-        Вернуться на главную
-      </div>
-
-      {(numberOfQuestions-counterOfCorrectAnswers)===0 ? (
-        <div
-          className={"big-font"}
-        >
-          WOW!  Вы знаток Манчестер Юнайтеда!
-        </div>
-      ):(
-        <div>
-
-          <div
-            onClick={() => showMistakes()}
-            className={"start-button"}
-          >
-            Показать мои ошибки
+            очков.
           </div>
 
-          {usersMistakes.map((item, id) => {
-            return (
+          <div
+            onClick={() => goBackToMain()}
+            className={"start-button"}
+          >
+            Вернуться на главную
+          </div>
+
+          {(numberOfQuestions - counterOfCorrectAnswers) === 0 ? (
+            <div
+              className={"big-font"}
+            >
+              WOW! Вы знаток Манчестер Юнайтеда!
+            </div>
+          ) : (
+            <div>
+
               <div
-                key={id}
-                className={"all-mistakes"}
+                onClick={() => showMistakes()}
+                className={"start-button"}
               >
-
-                <div
-                  className={"show-question"}
-                >
-                  {item.question}
-                </div>
-                <div
-                  className={"show-mistake"}
-                >
-                  {item.usersAns}
-                </div>
-                <div
-                  className={"show-answer"}
-                >
-                  {item.correctAns}
-                </div>
-
+                Показать мои ошибки
               </div>
-            );
-          })}
+
+              {usersMistakes.map((item, id) => {
+                return (
+                  <div
+                    key={id}
+                    className={"all-mistakes"}
+                  >
+
+                    <div
+                      className={"show-question"}
+                    >
+                      {item.question}
+                    </div>
+                    <div
+                      className={"show-mistake"}
+                    >
+                      {item.usersAns}
+                    </div>
+                    <div
+                      className={"show-answer"}
+                    >
+                      {item.correctAns}
+                    </div>
+
+                  </div>
+                );
+              })}
+            </div>
+          )}
+
+
+        </div>
+      ) : (
+        <div>
+          <ErrorPage/>
         </div>
       )}
-
-
     </div>
   )
 }
